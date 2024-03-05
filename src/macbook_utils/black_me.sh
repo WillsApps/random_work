135df1aa1376507bdd5a5e5ec98d89fb5bbf73b9
WillBurdettVerana <will.burdett@veranahealth.com>
2024-03-05 10:54:42 -0500

    Updating macbook scripts

--- src/macbook_utils/black_me.sh
+++ src/macbook_utils/black_me.sh
@@ -1,10 +1,12 @@
+#!/usr/bin/env bash
+
 REPO_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )/../.." &> /dev/null && pwd )
 source $REPO_DIR/venv/bin/activate
 
 export PYTHONPATH="$REPO_DIR/src/:$PYTHONPATH"
 
-if [ $# -eq 1 ]; then
-    black "$1"
+if [ $# != 0 ]; then
+    black "$*"
 else
     python $REPO_DIR/src/macbook_utils/black_me.py
 fi
--- src/macbook_utils/json_format.sh
+++ src/macbook_utils/json_format.sh
@@ -1,6 +1,8 @@
+#!/usr/bin/env bash
+
 REPO_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )/../.." &> /dev/null && pwd )
 source $REPO_DIR/venv/bin/activate
 
 export PYTHONPATH="$REPO_DIR/src/:$PYTHONPATH"
 
-python $REPO_DIR/src/macbook_utils/json_format.py "$1"
+python $REPO_DIR/src/macbook_utils/json_format.py "$*"
