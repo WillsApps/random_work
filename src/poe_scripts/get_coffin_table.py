eb0b8e677f3221c00da0dd1b0e2f92508eea006a
William Burdett <will@willsapps.com>
2024-05-12 12:46:29 -0400

    Move to main and black

--- src/poe_scripts/get_coffin_table.py
+++ src/poe_scripts/get_coffin_table.py
@@ -36,24 +36,26 @@ def get_modifier_name(name: str) -> str:
 def get_percent(name: str) -> int:
     return int(PERCENT_REGEX.findall(name)[0])
 
+
 def get_rows_newest_file(csv_folder: Path) -> list[dict[str, any]]:
-    exports = [file for file in csv_folder.iterdir() if "Wealthy-Exile-export" in file.name]
+    exports = [
+        file for file in csv_folder.iterdir() if "Wealthy-Exile-export" in file.name
+    ]
     exports.sort(key=os.path.getctime, reverse=True)
     file = exports[0]
     return pandas.read_csv(file).to_dict(orient="records")
 
 
-def get_modifiers(csv_data: list[dict]) -> dict[str,Modifier]:
+def get_modifiers(csv_data: list[dict]) -> dict[str, Modifier]:
     modifiers = {}
     for row in csv_data:
         name = row["Name"]
         if (
-            ("% increased chance of " not in name
+            "% increased chance of " not in name
             and "% increased chance for " not in name
             and "scarcer" not in name
             and "+50 to Modifier Tier" not in name
-            )
-                # or  "Corruption" in name
+            # or  "Corruption" in name
         ):
             continue
         if "+50 to Modifier Tier" in name:
@@ -76,7 +78,8 @@ def get_modifiers(csv_data: list[dict]) -> dict[str,Modifier]:
         modifiers[modifier_name] = modifier
     return modifiers
 
-def get_markdown_table(modifiers: dict[str,Modifier]) -> list[str]:
+
+def get_markdown_table(modifiers: dict[str, Modifier]) -> list[str]:
     table_lines = [
         (
             """|               | Increased | Scarcer |
@@ -95,15 +98,24 @@ def get_markdown_table(modifiers: dict[str,Modifier]) -> list[str]:
     )
     return table_lines
 
+
 def make_backup(original_file: Path, backup_file: Path):
     with open(original_file, "r") as original_f:
         with open(backup_file, "w") as backup_f:
             backup_f.write(original_f.read())
+
+
 def get_content_lines(original_file: Path) -> list[str]:
     with open(original_file, "r") as original_f:
         return original_f.read().split("\n")
 
-def splice_in_rows(new_lines: list[str], original_lines: list[str],start_line: str, end_line: str, ) -> list[str]:
+
+def splice_in_rows(
+    new_lines: list[str],
+    original_lines: list[str],
+    start_line: str,
+    end_line: str,
+) -> list[str]:
     start_line_number = 0
     end_line_number = 0
     for line_number, line in enumerate(original_lines):
@@ -113,19 +125,31 @@ def splice_in_rows(new_lines: list[str], original_lines: list[str],start_line: s
             end_line_number = line_number
 
     return (
-        original_lines[: start_line_number + 2] + new_lines + original_lines[end_line_number - 1 :]
+        original_lines[: start_line_number + 2]
+        + new_lines
+        + original_lines[end_line_number - 1 :]
     )
+
 
 def write_content_lines(target_file: Path, content: list[str]):
     with open(target_file, "w") as f:
-        f.write("\n".join(new_content))
+        f.write("\n".join(content))
 
 
-if __name__ == '__main__':
+def main():
     rows = get_rows_newest_file(CSV_FOLDER)
     modifiers = get_modifiers(rows)
     markdown_table = get_markdown_table(modifiers)
     original_content = get_content_lines(ORIGINAL_FILE)
     make_backup(ORIGINAL_FILE, BACKUP_FILE)
-    new_content = splice_in_rows(markdown_table,original_content, "-- Table Start", "-- Table End", )
+    new_content = splice_in_rows(
+        markdown_table,
+        original_content,
+        "-- Table Start",
+        "-- Table End",
+    )
     write_content_lines(ORIGINAL_FILE, new_content)
+
+
+if __name__ == "__main__":
+    main()
