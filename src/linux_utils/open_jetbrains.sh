7eee52e0a2acf0a3ef7ecc3b3f9214427d6c4032
Aggy <will@willsapps.com>
2024-12-16 15:25:00 -0500

    Update open_jetbrains

--- src/linux_utils/open_jetbrains.sh
+++ src/linux_utils/open_jetbrains.sh
@@ -9,11 +9,9 @@ cd "$DIR" || exit
 
 if [ "$PARENT_DIR" != "Code" ]
 then
-  screen -S pycharm -d -m "/home/aggy/Programs/$PROGRAM-$VERSION/bin/$PROGRAM"
+  screen -S pycharm -d -m "$HOME/Programs/$PROGRAM-$VERSION/bin/$PROGRAM"
   echo "Generically opening $PROGRAM"
   exit 0
 fi
 
-
-sc
-cd "$DIR" || exitreen -S "$PROGRAM-$DIR" -d -m "/home/aggy/Programs/$PROGRAM-$VERSION/bin/$PROGRAM" "$(pwd)"
+screen -S "$PROGRAM-$DIR" -d -m "$HOME/Programs/$PROGRAM-$VERSION/bin/$PROGRAM" "$(pwd)"
