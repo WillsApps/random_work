8fcb68f9bfa094c4e659f157d7a5cca98bb24b42
Aggy <will@willsapps.com>
2025-09-10 18:43:24 -0400

    Add scriptable data class

---
+++ src/general_utils/ScriptableDataClass.py
@@ -0,0 +1,15 @@
+import abc
+from typing import Mapping
+
+from dataclasses_json import DataClassJsonMixin
+
+
+class ScriptableDataClass(DataClassJsonMixin, Mapping, abc.ABC):
+    def __len__(self):
+        return len(self.to_dict())
+
+    def __iter__(self):
+        return iter(self.to_dict())
+
+    def __getitem__(self, key, default=None):
+        return self.to_dict().get(key, default)
